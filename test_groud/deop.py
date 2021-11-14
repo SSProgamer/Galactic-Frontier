a5c8cb18644ca55ebe780e862b91ca70ab7f308a
Thanawat sritho <64070044@kmitl.ac.th>
2021-11-14 23:05:05 +0700

    test menu and rotate

---
+++ test_groud/deop.py
@@ -0,0 +1,56 @@
+import pygame 
+import puy
+
+# Intialize the pygame
+pygame.init()
+# create screen
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Demo')
+background = pygame.image.load("main_game.png")
+rushia = pygame.image.load("tenor.gif")
+background_1_rect = rushia.get_rect(center = (400,300))
+start_img = pygame.image.load('start_btn.png').convert_alpha()
+exit_img = pygame.image.load('exit_btn.png').convert_alpha()
+
+#create button instances
+start_button = puy.Button(325, 300, start_img, 0.6)
+exit_button = puy.Button(330, 425, exit_img, 0.55)
+#game loop
+def rotate(rushia, angle):
+	rotated_surface = pygame.transform.rotozoom(rushia, angle, 1)
+	rotated_rect = rotated_surface.get_rect(center = (400,300))
+	return rotated_surface, rotated_rect
+run = True
+running = False
+angle = 1
+while run:
+
+	screen.fill((0, 0, 0))
+	screen.blit(background, (0, 0))
+	if start_button.draw(screen):
+		running = True
+		while running:
+			for event in pygame.event.get():
+			#quit game
+				
+				if event.type == pygame.QUIT:
+					run = False
+					running = False
+			screen.fill((0, 0, 52))
+			rushia_rotated, rushia_rect = rotate(rushia, angle)
+			screen.blit(rushia_rotated, rushia_rect)
+			if exit_button.draw(screen):
+					angle += 1
+			
+			pygame.display.update()
+	if exit_button.draw(screen):
+		run = False
+
+	#event handler
+	for event in pygame.event.get():
+		#quit game
+		if event.type == pygame.QUIT:
+			run = False
+
+	pygame.display.update()
+pygame.quit()
